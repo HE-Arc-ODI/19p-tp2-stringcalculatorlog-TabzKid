a7f62ae2065738d91ef5c5c5e940e6afb0a6f7ce
TabzKid <kidane.tabo@gmail.com>
2019-06-16 23:29:08 +0200

    log: Relates #1

--- src/main/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculator.java
+++ src/main/java/ch/hearc/ig/odi/stringcalculatortdd/StringCalculator.java
@@ -1,77 +1,85 @@
 package ch.hearc.ig.odi.stringcalculatortdd;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class StringCalculator {
 
-    public StringCalculator() {
+  private static Logger logger = LogManager.getLogger(StringCalculator.class);
+
+  public StringCalculator() {
+  }
+
+  public int add(final String numbers) {
+    //default delimiter
+    String delimiter = ",|\n";
+    String numbersUpdated = numbers;
+
+    //if it begins by '//' then it's a personalize delimiter(s)
+    if (numbers.startsWith("//")) {
+      delimiter = this.extractDelimiter(numbers);
+      //"cut" the beginning of the string so that the personalize delimiter is removed
+      numbersUpdated = numbers.substring(numbers.indexOf("\n") + 1);
     }
-    
-    public int add(final String numbers) {
-        //default delimiter
-        String delimiter = ",|\n";
-        String numbersUpdated = numbers;
+    return add(numbersUpdated, delimiter);
+  }
 
-        //if it begins by '//' then it's a personalize delimiter(s)
-        if (numbers.startsWith("//")) {
-            delimiter = this.extractDelimiter(numbers);
-            //"cut" the beginning of the string so that the personalize delimiter is removed
-            numbersUpdated = numbers.substring(numbers.indexOf("\n") + 1);
-        }
-        return add(numbersUpdated, delimiter);
-    }
+  private int add(final String numbersWithoutDelimiter, final String delimiter) {
+    int returnValue = 0;
+    List<Integer> negativeNumbers = new ArrayList();
 
-    private int add(final String numbersWithoutDelimiter, final String delimiter) {
-        int returnValue = 0;
-        List<Integer> negativeNumbers = new ArrayList();
+    //a string tab containing the numbers without the delimiter(s) is created
+    String[] numbersArray = numbersWithoutDelimiter.split(delimiter);
 
-        //a string tab containing the numbers without the delimiter(s) is created
-        String[] numbersArray = numbersWithoutDelimiter.split(delimiter);
-
-        for (String number : numbersArray) {
-            //test if the numbers array is not empty
-            if (!(number.trim().length() == 0)) {
-                Integer numberInt = Integer.parseInt(number);
-                if (numberInt < 0)
-                    negativeNumbers.add(numberInt);
-                else if (numberInt <= 1000)
-                    returnValue += numberInt;
-            }
-        }
-
-        if (negativeNumbers.size() > 0) {
-            throw new RuntimeException("Negative not allowed: " + negativeNumbers.toString());
-        }
-
-        return returnValue;
-    }
-
-    private String extractDelimiter(String numbers) {
-        String[] delimiters;
-        String returnDelimiter;
-        int beginDelimiterIndex = numbers.indexOf("//") + 2;
-        int endDelimiterIndex = numbers.indexOf("\n");
-        //if it begin with '[' and end by ']' there are multiple delimiters
-        if(numbers.charAt(beginDelimiterIndex) == '[' && numbers.charAt(endDelimiterIndex - 1) == ']') {
-            //use an Apache library to get each delimiters between '[' and ']'
-            delimiters = StringUtils.substringsBetween(numbers.substring(beginDelimiterIndex, endDelimiterIndex), "[", "]");
-            returnDelimiter = "";
-            //iterate through the delimiters list and concatenate the delimiter with a '|' between
-            //them so that the split method is OK with the regex
-            for (String element: delimiters) {
-                returnDelimiter += element;
-                returnDelimiter += "|";
-            }
-            returnDelimiter = StringUtils.removeEnd(returnDelimiter, "|");
+    for (String number : numbersArray) {
+      //test if the numbers array is not empty
+      if (!(number.trim().length() == 0)) {
+        Integer numberInt = Integer.parseInt(number);
+        if (numberInt < 0) {
+          logger.fatal("Nombre négatif : " + numberInt);
+          negativeNumbers.add(numberInt);
+        } else if (numberInt <= 1000) {
+          returnValue += numberInt;
         } else {
-            //there is only one delimiter
-            returnDelimiter = numbers.substring(beginDelimiterIndex, endDelimiterIndex);
+          logger.warn("Nombre plus grand que 1000 : " + numberInt);
         }
-
-        return returnDelimiter;
+      }
     }
+
+    if (negativeNumbers.size() > 0) {
+      throw new RuntimeException("Negative not allowed: " + negativeNumbers.toString());
+    }
+
+    return returnValue;
+  }
+
+  private String extractDelimiter(String numbers) {
+    String[] delimiters;
+    String returnDelimiter;
+    int beginDelimiterIndex = numbers.indexOf("//") + 2;
+    int endDelimiterIndex = numbers.indexOf("\n");
+    //if it begin with '[' and end by ']' there are multiple delimiters
+    if (numbers.charAt(beginDelimiterIndex) == '['
+        && numbers.charAt(endDelimiterIndex - 1) == ']') {
+      //use an Apache library to get each delimiters between '[' and ']'
+      delimiters = StringUtils
+          .substringsBetween(numbers.substring(beginDelimiterIndex, endDelimiterIndex), "[", "]");
+      returnDelimiter = "";
+      //iterate through the delimiters list and concatenate the delimiter with a '|' between
+      //them so that the split method is OK with the regex
+      for (String element : delimiters) {
+        returnDelimiter += element;
+        returnDelimiter += "|";
+      }
+      returnDelimiter = StringUtils.removeEnd(returnDelimiter, "|");
+    } else {
+      //there is only one delimiter
+      returnDelimiter = numbers.substring(beginDelimiterIndex, endDelimiterIndex);
+    }
+    logger.info("Délimiteur personnalisé : " + returnDelimiter);
+    return returnDelimiter;
+  }
 }
